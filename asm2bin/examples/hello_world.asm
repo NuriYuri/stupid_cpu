54db00cd55734052af8845fda63fb53c2b03004e
Nuri Yuri <qsdf@mlkjh.com>
2022-08-06 18:03:00 +0200

    Add forgotten instruction in example

--- asm2bin/examples/hello_world.asm
+++ asm2bin/examples/hello_world.asm
@@ -10,5 +10,6 @@ main:
   mov copyDataDestAddr, 0x0000 ; First address in VRAM 1 contains text to output to a 80x20 terminal (for now)
   mov copyDataDmaDest, 0x0010 ; VRAM 1
   sync copyDataDmaDest
+  dmaCopy ; Actually copies the data to the VRAM 
   lock currentDmaId
   sync currentDmaId ; Stops the program since the dma is locked by CPU and will never be released
